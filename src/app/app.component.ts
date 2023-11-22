69d54b9ee05c6e8b8ebf52b9062148ed5d5e17b7
tarun08pareta <tarun2001pareta@gmail.com>
2023-11-22 14:49:36 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,3 +12,27 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'ebbb';
 }
+ { function applyRandomEffect() {
+    const element = document.getElementById('digital');
+    const effects = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble', 'jello'];
+
+    // Randomly select an effect
+    const randomEffect = effects[Math.floor(Math.random() * effects.length)];
+
+    // Remove previous effects
+    element.classList.remove(...effects);
+
+    // Apply the random effect
+    element.classList.add(randomEffect);
+
+    // Set a timeout to remove the effect after 2 seconds (adjust as needed)
+    setTimeout(() => {
+        element.classList.remove(randomEffect);
+    }, 2000);
+}
+
+// Apply the random effect every 5 seconds (adjust as needed)
+setInterval(() => {
+    applyRandomEffect();
+}, 5000);
+}
